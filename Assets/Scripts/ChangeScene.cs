90186434d00108f86409739f159b63ae99cf149b
MANA0622\liker <likerng@outlook.jp>
2025-09-26 12:06:57 +0900

    細かい修正分

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -3,15 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-
+    public string sceneName;
     //シーンを切り替える機能をもったメソッド作成
-    public void OnTriggerEnter2D(Collision2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
             {
-            //引数に指定した名前のシーンに切り替えしてくれるメソッドの呼び出し
 
-            SceneManager.LoadScene("Boss");
+            SceneManager.LoadScene(sceneName);
         }
     }
 }
